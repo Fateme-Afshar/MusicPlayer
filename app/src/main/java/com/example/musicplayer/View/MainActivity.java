90a04845cf9f13a6f3aa8e8cd3dae318bf8053d1
FatemeAfshar20 <uttab4760@gmail.com>
2020-11-22 23:42:49 -0800

    Initial commit

---
+++ app/src/main/java/com/example/musicplayer/View/MainActivity.java
@@ -0,0 +1,12 @@
+package com.example.musicplayer.View;
+
+import androidx.fragment.app.Fragment;
+
+public class MainActivity extends SingleFragmentActivity {
+
+
+    @Override
+    public Fragment getFragment() {
+        return null;
+    }
+}
---
+++ app/src/main/java/com/example/musicplayer/ViewModel/MusicPlayerViewModel.java
@@ -0,0 +1,6 @@
+package com.example.musicplayer.ViewModel;
+
+import androidx.lifecycle.ViewModel;
+
+public class MusicPlayerViewModel extends ViewModel {
+}
